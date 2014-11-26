d7b5cc0e532c9784e083a9e56a58ec58ccb76f8e
Eric Walsh <ericmatthewwalsh@gmail.com>
2014-11-25 23:45:11 -0800

    Minor view changes and some routing fun

--- router.js
+++ router.js
@@ -4,4 +4,13 @@ router.get('/', function(req, res) {
 	res.render('index');
 });
 
+router.get('/msg/', function(req, res) {
+	res.send("Hello World spicy fries!");
+});
+
+
+router.use(function(req, res, next){
+  res.status(404).send("Sorry can't find that!");
+});
+
 module.exports = router;
